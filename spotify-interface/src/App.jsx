07c59bb1c75723d9184f74b55187437e486ac9a2
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-11 08:41:53 -0300

    componentes separados

--- spotify-interface/src/App.jsx
+++ spotify-interface/src/App.jsx
@@ -1,94 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Cardsidebar from "./components/Cardsidebar"
+import Container from "./components/Container"
+import ConteudoPrincipal from "./components/ConteudoPrincipal"
+import Header from "./components/Header"
+import Sidebar from "./components/Sidebar"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <nav className='w-[100%] h-[100px] bg-red-700'></nav>
+      <Header/>    
+      <Container> 
 
-      <div className='flex'>
-
-        <div className='w-2/12 h-[600px] bg-emerald-400 flex flex-col items-center justify-center'>
-            <div className='w-36 h-36 bg-red-700 m-5'></div>
-            <div className='w-36 h-36 bg-red-700 m-5'></div>
-            <div className='w-36 h-36 bg-red-700 m-5'></div>
-        </div>
-
-        <div className='grid grid-cols-4 place-items-center w-10/12 items-start mt-5'>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-            </div>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-            </div>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-            </div>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div></div>
-            </div>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-            </div>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-            </div>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-            </div>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div></div>
-            </div>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-            </div>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-            </div>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-            </div>
-
-            <div className='w-36 h-36 bg-red-700 m-4 flex flex-col items-center justify-center'>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div className='w-20 h-8 bg-emerald-400 m-2'></div>
-              <div></div>
-            </div>
-
-        </div>
-         
-      
-        </div>
-
-    
+        <Sidebar>
+          <Cardsidebar/>
+          <Cardsidebar/>
+          <Cardsidebar/>
+        </Sidebar>
+        
+        <ConteudoPrincipal/>
+      </Container>
     </>
   )
 }
