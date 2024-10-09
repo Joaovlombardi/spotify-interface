223716431e5e05f1379ce9e668ae840d05fd61cc
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-09 08:26:35 -0300

    mapeando

--- spotify-interface/src/App.jsx
+++ spotify-interface/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import Cardsidebar from "./components/Cardsidebar"
 import Container from "./components/Container"
 import ConteudoPrincipal from "./components/ConteudoPrincipal"
@@ -5,6 +6,16 @@ import Header from "./components/Header"
 import Sidebar from "./components/Sidebar"
 
 function App() {
+
+  const [artistas, setArtistas] = useState([]);
+  const [isLoding, setIsLoding] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/artistas')
+      .then(res => res.json())
+      .then(data => setArtistas(data))
+      .catch(err => console.log(err))
+  }, [])
 
   return (
     <>
@@ -17,7 +28,18 @@ function App() {
           <Cardsidebar/>
         </Sidebar>
         
-        <ConteudoPrincipal/>
+        <ConteudoPrincipal>
+        {
+            artistas.map( artista => (
+              <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+              <p>{artista.name}</p>
+              <div className="bg-green-400 w-3/4 h-7"></div>
+            </div>
+            ))
+        }
+
+        </ConteudoPrincipal>
+
       </Container>
     </>
   )
