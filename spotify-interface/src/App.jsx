8e5e40795c849d05946c928fb0804fb2c7b4af30
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-16 08:46:58 -0300

    filter feito

--- spotify-interface/src/App.jsx
+++ spotify-interface/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       .then(res => res.json())
       .then(data => setArtistas(data))
       .catch(err => console.log(err))
+      .finally(() => console.log('Finalizou a requisição'))
   }, [])
 
   return (
@@ -29,10 +30,40 @@ function App() {
         </Sidebar>
         
         <ConteudoPrincipal>
-        {
-            artistas.map( artista => (
+        <h1 className="">Rap</h1>
+        {artistas
+            .filter( artista => artista.genero.
+              includes("Rap"))
+            .map( artista => (
               <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
               <p>{artista.name}</p>
+              <p>{artista.genero}</p>
+              <div className="bg-green-400 w-3/4 h-7"></div>
+            </div>
+            ))
+        }
+
+        <h1>Pop</h1>
+        {artistas
+            .filter( artista => artista.genero.
+              includes("Pop"))
+            .map( artista => (
+              <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+              <p>{artista.name}</p>
+              <p>{artista.genero}</p>
+              <div className="bg-green-400 w-3/4 h-7"></div>
+            </div>
+            ))
+        }
+
+        <h1>Rock</h1>
+        {artistas
+            .filter( artista => artista.genero.
+              includes("Rock"))
+            .map( artista => (
+              <div className="bg-red-500 w-28 h-28 flex flex-col justify-around items-center">
+              <p>{artista.name}</p>
+              <p>{artista.genero}</p>
               <div className="bg-green-400 w-3/4 h-7"></div>
             </div>
             ))
